10505c41de45f961e3c33d52ccbe47d449ad66d6
Aman Tiwari <80471500+aman-ama-Au17@users.noreply.github.com>
2022-01-23 20:59:03 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 // app.use(require('body-parser').json())
 
 
-app.use("/products", productController)
+app.use("/", productController)
 app.use("/users", userController)
 
 module.exports = app
